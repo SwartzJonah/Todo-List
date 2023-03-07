7587c6ba7816f6898d46cc9e3029802a9fd8ce6f
SwartzJonah <swartzjonah@gmail.com>
2023-03-06 20:24:38 -0500

    added a project array that holds projects

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 !console.log("hello");
 //import { compareAsc, format } from 'date-fns';
 import { todoFactory } from "./modules/todo";
-import { projectFactory } from "./modules/project";
+import { projectFactory, projectsHolder } from "./modules/project";
 
 
 const dance = todoFactory("dancing", "1/7");
 const project = projectFactory("yahoo", [2,3], "bigdance")
 
-dance.sayTitle();
-project.showTodo();
+console.log(dance);
+console.log(project);
+console.log(projectsHolder);
--- src/modules/project.js
+++ src/modules/project.js
@@ -3,3 +3,11 @@ export const projectFactory = (title, todoList, description) => {
     const showTodo = () => console.log(todoList);
     return {title, todoList, description, showTodo};
 }
+
+const defaultProject = projectFactory("Default", [], "Default Project");
+
+export const projectsHolder = (() => {
+    const projectsArray = [];
+    projectsArray.push(defaultProject);
+    return { projectsArray};
+})();
